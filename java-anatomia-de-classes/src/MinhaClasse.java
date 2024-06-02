a52ed2a5266022d738ac5585bb6db15bb4e9f6d9
claudioteo01 <klaudioteo@gmail.com>
2024-06-02 10:55:15 -0300

    Boletim Estudante

--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -2,8 +2,21 @@ public class MinhaClasse {
     
 
     public static void main (String [] args ){
-        System.out.print("Olá turma, sejam bem vindos");
 
+        String primeiroNome = "Claudio";
+        String segundoNome = "Teodoro";
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        
+        System.out.println(nomeCompleto);
+       
     }
+
+       public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+            return "Resultado do metodo nome completo:  "+ primeiroNome.concat(" ").concat(segundoNome);
+
+       }
+
+    
 
 }
