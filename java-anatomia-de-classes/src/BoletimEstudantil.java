565e060ce8425b5446b046c26f8acfbc3591aad0
claudioteo01 <klaudioteo@gmail.com>
2024-06-02 11:00:48 -0300

    Boletim versão 2.0

--- java-anatomia-de-classes/src/BoletimEstudantil.java
+++ java-anatomia-de-classes/src/BoletimEstudantil.java
@@ -1,12 +1,12 @@
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 3;
-        if(mediaFinal<6)
-        System.out.println("Reprovado");
+        if(mediaFinal  < 6 )
+            System.out.println("Reprovado");
         else if(mediaFinal==6)
-        System.out.println("Prova minerva");
+            System.out.println("Prova minerva");
         else
-        System.out.println("Aprovado");
+            System.out.println("Aprovado");
         
     }
 
