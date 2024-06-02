9afe2fd6920311806fbd62444d15cf6c26db68ba
claudioteo01 <klaudioteo@gmail.com>
2024-06-02 11:16:50 -0300

    cor carro 2

---
+++ java-anatomia-de-classes/src/CorCarro.java
@@ -0,0 +1,14 @@
+public class CorCarro {
+    public static void main(String[] args) {
+        var corCarro = "Branco";
+        if(corCarro  == "Vermelho" )
+            System.out.println("Vermelho");
+        else if(corCarro=="Azul")
+            System.out.println("Carro e azul");
+        else
+            System.out.println("Cor nao identificada");
+        
+    }
+
+    
+}
